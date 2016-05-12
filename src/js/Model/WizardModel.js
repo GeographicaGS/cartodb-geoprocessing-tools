e0c17ef7fbdd6f103e857071141cb11f3a1ac9d0
Raúl Yeguas <neokore@gmail.com>
2016-05-12 08:42:12 +0200

    Fix parse CartoCSS string [#119399409]

--- src/js/Model/WizardModel.js
+++ src/js/Model/WizardModel.js
@@ -27,7 +27,8 @@ App.Model.Wizard.CartoCSS = Backbone.Model.extend({
   },
 
   loadCartoCSS: function(cartoCSSString){
-    var re = /([\w-]+):\s?([#A-Za-z0-9-.]*)/g;
+    // var re = /([\w-]+):\s?([#A-Za-z0-9-.]*)/g;
+    var re = new RegExp("([\\w-]+):\\s?([#A-Za-z0-9-.]*)", "g");
     while((result = re.exec(cartoCSSString)) !== null) {
       var property = result[1].replace(/-/g, '_');
       if(this.get(property)){
