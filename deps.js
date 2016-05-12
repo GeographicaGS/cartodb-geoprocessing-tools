cbbef5fa3138c337a1bad4183ff30323e7d87037
Alberto Asuero <alberto.asuero@geographica.gs>
2016-05-12 08:12:06 +0200

    Trying to use clipboard.js

--- deps.js
+++ deps.js
@@ -11,6 +11,7 @@ deps.JS = [
   srcJS + 'lib/jquery-2.1.3.js',
   srcJS + 'lib/underscore-1.8.2.js',
   srcJS + 'lib/backbone-1.1.2.js',
+  srcJS + 'lib/clipboard.js',
 
   // Namespace
   srcJS + 'Namespace.js',
